66f74312ad6b4362196d525c78542a4048cbc052
Taehee1004 <87217639+Taehee1004@users.noreply.github.com>
2021-08-11 20:09:12 +0900

    Update face_crop_vid.py

--- 2. Preprocessing/face_crop_vid.py
+++ 2. Preprocessing/face_crop_vid.py
@@ -1,48 +1,52 @@
-import cv2
-import glob
-import dlib
-import numpy as np
-import os
-import imutils
-from matplotlib import pyplot as plt
-from imutils import face_utils
-
-
-def face_crop_vid (MOV_Directory):
+def face_crop_vid (Clean_File_name):
     
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    MOV_List = glob.glob(MOV_Directory + '*.mp4')
-    MOV_List = os.listdir(MOV_List)
+    File_name = './CUT_VID/' + Clean_File_name + '.mp4'
+    video =  cv2.VideoCapture(File_name)
 
-    for MOV in MOV_List:
-        MOV_name = MOV_Directory + '/' + MOV
-
-        video =  cv2.VideoCapture(MOV_name)
-
-        fps = video.get(cv2.CAP_PROP_FPS)
-        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-        height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        size = (round(width), round(height))
-        out = cv2.VideoWriter('./FINAL_VID/%s.mp4' %MOV, fourcc, fps, size)
-
-        while(video.isOpened()):
-        
-            ret, frame = video.read()
-            
-            if frame is None:
-                break
-
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, 1.25, 6)
-
-            for (x, y, w, h) in faces:
-                cropped = frame[y - int(h/4):y + h + int(h/4), x - int(w/4):x + w + int(w/4)]
-
-
-            out.write(cropped)
-
-        out.release()
+    fps = video.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    size = (round(width), round(height))
+    frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
     
-    return './FINAL_VID'
+    cnt = 0
+    
+    
+    out = cv2.VideoWriter('./FINAL_VID/%s.mp4' %Clean_File_name, fourcc, fps, size)
+
+    while(video.isOpened()):
+    
+        ret, frame = video.read()
+        
+        cnt += 1
+        
+        if frame is None:
+            break
+
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 
+                                              scaleFactor=1.2, 
+                                              minNeighbors=5, 
+                                              minSize=(20,20))
+        if ret == True:
+            
+            for (x, y, w, h) in faces:
+                track_window = (x, y, w, h)
+            
+                x, y, w, h = track_window
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0, 0), 2)
+                dst = np.zeros_like(frame)
+                dst[y:y+h,x:x+w] = frame[y:y+h,x:x+w]
+                
+            xx = cnt * 100/frames
+
+
+        out.write(dst)
+
+    video.release()
+    out.release()
+    cv2.destroyAllWindows()
